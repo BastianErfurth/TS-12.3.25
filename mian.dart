669d07493be5021bf29f6f8f6c820a36b0295b68
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-03-12 10:58:14 +0100

    Aufgabe 2 gelöst

--- mian.dart
+++ mian.dart
@@ -1,9 +1,20 @@
-void printNumber(int x) {
-  print(x);
+// Aufgabe 1
+void printNumber(int wert) {
+  print(wert);
+}
+
+// Aufgabe 2
+
+void triplePrint(String text) {
+  print(text);
+  print(text);
+  print(text);
 }
 
 void main() {
   printNumber(4);
   printNumber(9);
   printNumber(45);
+
+  triplePrint("Funktionen sind ne coole Sache!");
 }
